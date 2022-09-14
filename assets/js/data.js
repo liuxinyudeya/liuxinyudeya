cf15f744132465b37e1192074c9c65ea95723713
liuxy <liuxinyudeya2018@163.com>
2022-09-14 23:20:11 +0800

    change

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,57 +3,39 @@ var shopList =
         "lycList": [
             {
                 "id": "lyc-1",
+                "type": "lyc",
                 "png": "1.png",
-                "name": "Google",
+                "name": "露营车1号",
+                "content": "this is content deatil ...",
+                "sale_state": "有货",
                 "zj_price": "10",
                 "yj_price": "500"
             }, {
                 "id": "lyc-2",
+                "type": "lyc",
                 "png": "2.png",
-                "name": "Google",
+                "name": "露营车2号",
+                "content": "this is content deatil ...",
+                "sale_state": "售罄",
                 "zj_price": "20",
                 "yj_price": "500"
             }, {
                 "id": "lyc-3",
+                "type": "lyc",
                 "png": "3.png",
-                "name": "Google",
+                "name": "露营车3号",
+                "content": "this is content deatil ...",
+                "sale_state": "售罄",
                 "zj_price": "30",
                 "yj_price": "500"
             }, {
                 "id": "lyc-4",
+                "type": "lyc",
                 "png": "4.png",
-                "name": "Google",
+                "name": "露营车4号",
+                "content": "this is content deatil ...",
+                "sale_state": "售罄",
                 "zj_price": "40",
-                "yj_price": "500"
-            }, {
-                "id": "lyc-5",
-                "png": "5.png",
-                "name": "Google",
-                "zj_price": "50",
-                "yj_price": "500"
-            }, {
-                "id": "lyc-6",
-                "png": "6.png",
-                "name": "Google",
-                "zj_price": "60",
-                "yj_price": "500"
-            }, {
-                "id": "lyc-7",
-                "png": "7.png",
-                "name": "Google",
-                "zj_price": "70",
-                "yj_price": "500"
-            }, {
-                "id": "lyc-8",
-                "png": "8.png",
-                "name": "Google",
-                "zj_price": "80",
-                "yj_price": "500"
-            }, {
-                "id": "lyc-9",
-                "png": "8.png",
-                "name": "Google",
-                "zj_price": "90",
                 "yj_price": "500"
             }
         ]
--- assets/js/lazycamping.js
+++ assets/js/lazycamping.js
@@ -7,34 +7,37 @@ window.onload = function () {
     for (let i = 0; i < lycsize; i++) {
         //
         let lyc = lycList[i]
-        let lycItem = ` <div class="item">
-                                <div class="product-box">
-                                    <div class="product-media">
-                                        <img class="prod-img" alt="" src="assets/img/products/${lyc.png}"/>
-                                        <img class="shape" alt="" src="assets/img/icons/shap-small.png"/>
-                                        <div class="prod-icons">
-                                            <a class="fa fa-heart" onclick="clickLike(this)" id="${lyc.id}">1</a>
-                                            <a href="#" class="fa fa-shopping-basket">2</a>
-                                            <a href="#product-preview" data-toggle="modal" class="fa fa-expand">3</a>
-                                        </div>
-                                    </div>
-                                    <div class="product-caption">
-                                        <h3 class="product-title">
-                                            <a href="#"> <span class="light-font"> ${lyc.name} </span>
-                                                <strong>tomato</strong></a>
-                                        </h3>
-                                        <div class="price">
-                                            <strong class="clr-txt">￥${lyc.zj_price} </strong>
-                                        </div>
-                                    </div>
+        let lycItem = `<div class="item">
+                    <div class="deal-item">
+                        <div class="deal-icons">
+                            <a href="#" class="fa fa-heart"></a>
+                            <a href="#" class="fa fa-shopping-basket"></a>
+                            <a href="#product-preview" data-toggle="modal" class="fa fa-expand"></a>
+                        </div>
+                        <div class="deal-content">
+                            <div class="text-right">
+                                <span class="prod-tag tag-1">new</span> <span class="prod-tag tag-2">sale</span>
+                            </div>
+                            <div class="deal-text">
+                                <h4 class="sub-title"> ORGANIC FRUITS </h4>
+                                <h2 class="fsz-30 pb-15"><a href="#"> <span class="light-font">fresh </span> <strong>rasberry </strong>
+                                </a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy...</p>
+                                <div class="price pt-15">
+                                    <strong class="clr-txt">$50.00 </strong>
+                                    <del class="light-font">$65.00</del>
                                 </div>
-                            </div>`
+                            </div>
+                            <div class="deal-img"><img alt="" src="assets/img/extra/deal-1.png"/></div>
+                        </div>
+                    </div>
+                </div>`
         lycItemList += lycItem
 
     }
 
 
-    shopMenu_1.innerHTML = lycItemList
+    shopMenu_1.innerHTML =  lycItemList
 
 
 }
@@ -43,4 +46,5 @@ function clickLike(data) {
     console.log("clickLike")
     console.log(data.id)
 }
+
 
